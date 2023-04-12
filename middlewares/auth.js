01474436cc867cf846c09f1609f3a651498b909d
Sivkov Ilya <sivkov11@yandex.ru>
2023-04-12 13:40:22 +0700

    пр14. тесты

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,7 +5,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new UnauthorizedError('Неправильные почта или пароль1');
+    res.status(401).send({ message: 'Необходима авторизация'})
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -14,7 +14,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, 'super-strong-secret');
   } catch (err) {
-    return next(new UnauthorizedError('Неправильные почта или пароль2'));
+    return next(new UnauthorizedError('Необходима авторизация'));
   }
 
   req.user = payload;
