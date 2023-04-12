f7f627438aa3dd7d33f10b6b74ae4d5b75c7a97b
Sivkov Ilya <sivkov11@yandex.ru>
2023-04-12 17:18:25 +0700

    пр14. тесты

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,7 +5,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return next(new UnauthorizedError('Необходима авторизация1'));
+    throw new UnauthorizedError('Необходима авторизация1');
   }
 
   const token = authorization.replace('Bearer ', '');
