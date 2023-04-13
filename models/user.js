f3f178695551bf7a63abccc172774f7725cd17e4
Sivkov Ilya <sivkov11@yandex.ru>
2023-04-13 16:18:32 +0700

    пр14. тесты

--- models/user.js
+++ models/user.js
@@ -37,26 +37,24 @@ const userSchema = new mongoose.Schema({
     required: true,
     select: false,
   },
+});
 
-  statics: {
-    findUserByCredentials(email, password) {
-      return this.findOne({ email })
-        .then((user) => {
-          if (!user) {
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
             return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
           }
 
-          return bcrypt.compare(password, user.password)
-            .then((matched) => {
-              if (!matched) {
-                return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
-              }
-
-              return user;
-            });
+          return user;
         });
-    },
-  },
-});
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
